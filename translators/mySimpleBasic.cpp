f61d0b53c3b12c0de3039acd608064c5b6245e8e
Denis Kuzmenok <kuzmenok.den@yandex.ru>
2023-06-18 18:48:17 +0700

    Final

--- translators/mySimpleBasic.cpp
+++ translators/mySimpleBasic.cpp
@@ -169,13 +169,13 @@ parsRPN (char *rpn, string var)
                 case '+':
 				  resComand.emplace_back(comandSimpleAStruct(curentComand++, "ADD ", variable[operand2].addres));
                   break;
-                case '-': // SUB
+                case '-': 
 				  resComand.emplace_back(comandSimpleAStruct(curentComand++, "SUB ", variable[operand2].addres));
                   break;
-                case '/': // DIVIDE
+                case '/': 
 				  resComand.emplace_back(comandSimpleAStruct(curentComand++, "DIVIDE ", variable[operand2].addres));
                   break;
-                case '*': // MUL
+                case '*':
 				  resComand.emplace_back(comandSimpleAStruct(curentComand++, "MUL ", variable[operand2].addres));
                   break;
                 }
@@ -220,31 +220,23 @@ checkPriority (char sign)
 
 char * translateToRPN (string inf, char *rpn)
 {
-  // Создание корневого узла стека
   stack<char> st;
   int i = 0;
   int j = 0;
 
-  // Пока не достигнут конец строки и не встречен символ новой строки
   while (inf[i] != '\0' && inf[i] != '\n')
     {
       char x = inf[i];
 
-      // Если встречен операнд, добавляем его в выходную строку
       if ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z') || (x >= '1' && x <= '9'))
         {
           rpn[j] = x;
           j++;
         }
-
-      // Если встречена открывающая скобка, добавляем ее в стек
       else if (x == '(')
         {
 			st.push(x);
         }
-
-      // Если встречена закрывающая скобка, извлекаем элементы из стека, пока
-      // не достигнем открывающую скобку и добавляем их в выходную строку
       else if (x == ')')
         {
           while (st.top() != '(')
@@ -257,13 +249,8 @@ char * translateToRPN (string inf, char *rpn)
                   j++;
                 }
             }
-          // Извлекаем открывающую скобку из стека
 			st.pop();
         }
-
-      // Если встречен оператор, извлекаем операторы из стека, пока их
-      // приоритет не станет меньше приоритета текущего оператора, и добавляем
-      // их в выходную строку
       else if (x == '+' || x == '-' || x == '*' || x == '/')
         {
           while (st.size() != 0 && checkPriority (st.top()) >= checkPriority (x))
@@ -276,14 +263,11 @@ char * translateToRPN (string inf, char *rpn)
                   j++;
                 }
             }
-          // Добавляем текущий оператор в стек
 		  st.push(x);
         }
       i++;
     }
 
-  // Извлекаем все оставшиеся операторы из стека и добавляем их в выходную
-  // строку
   while (st.size() != 0)
     {
 	  char c = st.top();
@@ -295,7 +279,6 @@ char * translateToRPN (string inf, char *rpn)
         }
     }
 
-  // Проверяем, что выходная строка корректно сформирована
   for (int k = 0; k < j; k++)
     {
       if (rpn[k] == '(' || rpn[k] == ')')
@@ -305,7 +288,6 @@ char * translateToRPN (string inf, char *rpn)
         }
     }
 
-  // Добавляем нуль-терминальный символ в конец выходной строки
   rpn[j] = '\0';
 
   while(!st.empty())
